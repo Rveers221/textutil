c687fe63d6c6200b269e4dc741cab1bd8198c190
Rveers221 <111116800+Rveers221@users.noreply.github.com>
2022-08-12 15:55:16 +0530

    Updates

---
+++ precache-manifest.ae6c54e7a7e8b3652982b5874a2c3d8e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "51dd11802677ed9ebf5b",
+    "url": "/textutils/static/css/main.f1ee7c21.chunk.css"
+  },
+  {
+    "revision": "51dd11802677ed9ebf5b",
+    "url": "/textutils/static/js/main.51dd1180.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/textutils/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/textutils/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "8c5d3a7ccc996c2dc493",
+    "url": "/textutils/static/js/runtime~main.8c5d3a7c.js"
+  },
+  {
+    "revision": "504f6a67b1b1c7309ef8ee9f7e203916",
+    "url": "/textutils/index.html"
+  }
+];
